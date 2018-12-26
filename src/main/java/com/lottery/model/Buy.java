b01dad63502dde927853ff42018b0f1df1532543
杨翼驹 <yyj_9-00618@163.com>
2018-12-26 14:32:35 +0800

    增加商家后台

---
+++ src/main/java/com/lottery/model/Buy.java
@@ -0,0 +1,105 @@
+package com.lottery.model;
+
+import java.util.Date;
+
+public class Buy {
+    private Integer id;
+
+    private Integer userid;
+
+    private Integer unitid;
+
+    private Date buydate;
+
+    private Date expiredate;
+
+    private Integer lotteryid;
+
+    private Integer productid;
+
+    private String ordernum;
+
+    private Integer ispay;
+
+    private Integer shareid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public Integer getUnitid() {
+        return unitid;
+    }
+
+    public void setUnitid(Integer unitid) {
+        this.unitid = unitid;
+    }
+
+    public Date getBuydate() {
+        return buydate;
+    }
+
+    public void setBuydate(Date buydate) {
+        this.buydate = buydate;
+    }
+
+    public Date getExpiredate() {
+        return expiredate;
+    }
+
+    public void setExpiredate(Date expiredate) {
+        this.expiredate = expiredate;
+    }
+
+    public Integer getLotteryid() {
+        return lotteryid;
+    }
+
+    public void setLotteryid(Integer lotteryid) {
+        this.lotteryid = lotteryid;
+    }
+
+    public Integer getProductid() {
+        return productid;
+    }
+
+    public void setProductid(Integer productid) {
+        this.productid = productid;
+    }
+
+    public String getOrdernum() {
+        return ordernum;
+    }
+
+    public void setOrdernum(String ordernum) {
+        this.ordernum = ordernum == null ? null : ordernum.trim();
+    }
+
+    public Integer getIspay() {
+        return ispay;
+    }
+
+    public void setIspay(Integer ispay) {
+        this.ispay = ispay;
+    }
+
+    public Integer getShareid() {
+        return shareid;
+    }
+
+    public void setShareid(Integer shareid) {
+        this.shareid = shareid;
+    }
+}
